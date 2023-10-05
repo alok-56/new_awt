7c3a3e24eff95fdd7a4be62ee89d0ac243eedacb
alok-56 <alokkumar11746@gmail.com>
2023-10-05 14:46:12 +0530

    alok

--- Pdfmailer.js
+++ Pdfmailer.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const nodemailer = require("nodemailer");
 
 async function generatePDF(data) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
   // Load the HTML template and replace placeholders with data
